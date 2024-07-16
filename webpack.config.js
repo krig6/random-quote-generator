daab649c1f8564d265936a54632f4ce115943122
krig6 <ej.ldsm@gmail.com>
2024-07-17 02:40:04 +0800

    Configure webpack for production and add deployment script for GitHub Pages
    - Change the mode to 'production'
    - Add 'gh-pages' to 'devDependencies'
    - Add a 'deploy' script

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
